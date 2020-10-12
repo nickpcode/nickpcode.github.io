613dfafbc8b05144fb6f78cbecef15589d121d4d
Nicholas Pretel <nicholaspretel@Nicholass-MBP.lan>
2020-10-12 16:17:45 -0400

    DOM setup

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,13 @@
 $(() => {
+    const words = ['air', 'answer', 'being', 'miss', 'year', 'I', 'move', 'eat', 'always', 'away', 'sun', 'get', 'great', 'air', 'page', 'feet', 'different', 'had', 'without', 'study', 'mile', 'think', 'need', 'let', 'mean', 'went', 'almost', 'to', 'how', 'them', 'upon', 'when', 'kind', 'it', 'are', 'about', 'call', 'food', 'air', 'or', 'example', 'state', 'cut', 'may', 'hand', 'would', 'should', 'work', 'good', 'sun', 'form', 'see', 'also', 'different', 'grow', 'get', 'few', 'began', 'find', 'no', 'carry', 'sun', 'through', 'say', 'but', 'those', 'been', 'follow', 'see', 'most', 'point', 'call', 'small', 'back', 'another', 'he', 'that', 'answer', 'time', 'plant', 'went', 'pizazz', 'piazzas', 'pizzas', 'suburban', 'assuming', 'obstinance', 'foramens']
+    const $prompt = $('<div>').addClass('prompt').text(words[Math.floor(Math.random()*(words.length))])
     const $input  = $('<input>').addClass('input')
     const $submit = $('<button>').addClass('submit').text('submit')
     $input.appendTo($('body'))
     $submit.appendTo($('body'))
+    $prompt.appendTo($('body'))
+
+    const submit =()=>{
+        
+    }
 })
